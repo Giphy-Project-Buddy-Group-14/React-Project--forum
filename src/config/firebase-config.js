53c7f7f10bd171ca2e733fd0ddb9f8903b0f1cc9
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2023-11-03 21:27:20 +0200

    making firebase config

---
+++ src/config/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBel8sDIqzDvtCi4jBVYJC86kXvzpme8OM",
+  authDomain: "react-project--forum.firebaseapp.com",
+  projectId: "react-project--forum",
+  storageBucket: "react-project--forum.appspot.com",
+  messagingSenderId: "586380113779",
+  appId: "1:586380113779:web:51613b3740ddc08acdf706",
+  databaseURL: "https://react-project--forum-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
