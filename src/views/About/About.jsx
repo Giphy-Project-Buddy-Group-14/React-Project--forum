4b2a7b81415c1172cc7f9b9bbd44feacba4664de
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2023-11-03 09:16:57 +0200

    update structure project forum

---
+++ src/views/About/About.jsx
@@ -0,0 +1,12 @@
+import './About.css';
+
+export default function About() {
+
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a forum app that will show you the post in your area.</p>
+        </div>
+    )
+
+}
---
+++ src/views/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <>
+        <footer className="footer">&copy; Forum App 2023</footer>
+        </>
+    )
+}
