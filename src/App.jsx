b80023caea6b2489681c86f886a33d9110638352
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2023-11-04 15:00:14 +0200

    create router and signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './views/Home/Home.jsx'
+import Signin from './components/Signin/Signin.jsx'
 
 function App() {
 
 
   return (
     <>
-     
+    <Routes>
+      <Route path='/home' element={<Home />}/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/signin' element={<Signin />}/>
+
+    
+
+    </Routes>
+     <h1>Hello Home Page</h1>
     </>
   )
 }
