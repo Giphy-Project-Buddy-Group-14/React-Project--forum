1183527bc770a704deddedd2630e95f565c9d9e9
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2023-11-05 14:54:38 +0200

    partial sign-in and ready auth context

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,92 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Signin from './components/Signin/Signin.jsx';
 import Signup from './components/Signup/Signup.jsx';
 import AuthLayout from './views/AuthLayout/AuthLayout.jsx';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './config/firebase-config.js';
+import { useEffect, useState } from 'react';
+import { getUserData } from './services/users.services.js';
+import { useToast } from './components/ui/use-toast.js';
+import { AuthContext } from './context/AuthContext.jsx';
 
 function App() {
+
+  const { toast } = useToast();
+  const [user] = useAuthState(auth);
+  const [appState, setAppState] = useState({
+    user,
+    userData: false,
+  })
+
+  if (appState.user !== user) {
+    setAppState({ user });
+  }
+
+
+  useEffect(() => {
+    if (user === null) {
+      return;
+    }
+    (async () => {
+      try {
+        const result = await getUserData(user.uid)
+        setAppState({
+          ...appState,
+          userData: Object.keys(result.val())[0]
+        })
+        throw new Error("Test");
+      } catch (error) {
+        toast({
+          variant: "destructive",
+          title: "Error authentication",
+          description: error.message,
+        })
+      }
+    })();
+  }, [])
+
+
+
   const location = useLocation();
   const authRoutes = ['/sign-in', '/sign-up'];
   const showNavBar = !authRoutes.includes(location.pathname);
 
   return (
     <>
-      {showNavBar && <NavBar />}
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/home"
-          element={<Home />}
-        />
-        <Route element={<AuthLayout />}>
-          <Route path='/sign-in' element={<Signin />} />
-          <Route path='/sign-up' element={<Signup />} />
-        </Route>
-        <Route
-          path="/forum"
-          element={<Forum />}
-        />
-        <Route
-          path="/about"
-          element={<About />}
-        />
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-        <Route
-          path="/settings"
-          element={<Settings />}
-        />
-      </Routes>
-      <Footer/>
+      <AuthContext.Provider value={{ ...appState, setUser: setAppState }}>
+        {showNavBar && <NavBar />}
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+          <Route element={<AuthLayout />}>
+            <Route path='/sign-in' element={<Signin />} />
+            <Route path='/sign-up' element={<Signup />} />
+          </Route>
+          <Route
+            path="/forum"
+            element={<Forum />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+          <Route
+            path="/settings"
+            element={<Settings />}
+          />
+        </Routes>
+        <Footer />
+      </AuthContext.Provider>
     </>
   );
 }
