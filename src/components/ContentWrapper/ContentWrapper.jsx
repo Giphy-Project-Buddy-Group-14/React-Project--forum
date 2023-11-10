aac9f94da0bf7ddeac1e0cbc03b973b07a8d682d
Svilena Slavev <svilslavev@gmail.com>
2023-11-11 00:54:56 +0200

    Footer CSS

--- src/components/ContentWrapper/ContentWrapper.jsx
+++ src/components/ContentWrapper/ContentWrapper.jsx
@@ -1,6 +1,6 @@
 export default function ContentWrapper(props) {
   return (
-    <div className="bg-white py-5 sm:py-5">
+    <div className="py-5 sm:py-5">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">{props.children}</div>
       </div>
