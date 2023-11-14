2bd879a8e8702fb3d6bb0194a5a73c4aaa7f1479
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2023-11-14 13:27:03 +0200

    Image ref used by Vite

--- src/views/NavBar/NavBar.jsx
+++ src/views/NavBar/NavBar.jsx
@@ -8,6 +8,7 @@ import { AuthContext } from '@/context/AuthContext.jsx';
 import { logoutUser } from '@/services/auth.services.js';
 import { useToast } from '@/components/ui/use-toast.js';
 import { Button } from '@/components/ui/button.jsx';
+import logoImage from '@/assets/logo.png';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -74,7 +75,7 @@ export default function NavBar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="/src/assets/logo.png"
+                    src={logoImage}
                     alt="Your Company"
                   />
                 </div>
