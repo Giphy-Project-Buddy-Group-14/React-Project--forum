aa29569cec29e009ae8bdd0e8b0ca83f2a515af7
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2023-11-14 12:10:28 +0200

    Removed whitespace. Fixed component format

--- src/components/PostExplorer/PostList.jsx
+++ src/components/PostExplorer/PostList.jsx
@@ -1,53 +1,67 @@
-import { getAllPosts, getMostCommentedPosts } from '@/services/post.services.js';
-import { useContext, useEffect, useState } from 'react'
+import {
+  getAllPosts,
+  getMostCommentedPosts,
+} from '@/services/post.services.js';
+import { useContext, useEffect, useState } from 'react';
 import { useToast } from '../ui/use-toast.js';
 import PostCard from './PostCard.jsx';
 import { Button } from '../ui/button.jsx';
 import { AuthContext } from '@/context/AuthContext.jsx';
 
 export default function PostList() {
+  const [posts, setPosts] = useState([]);
+  const [showCount, setShowCount] = useState(10);
+  const { toast } = useToast();
+  const { isLoggedIn } = useContext(AuthContext);
 
-    const [posts, setPosts] = useState([]);
-    const [showCount, setShowCount] = useState(10); // New state for number of posts to show
-    const { toast } = useToast();
-    const { isLoggedIn } = useContext(AuthContext);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getAllPosts();
+        setPosts(response);
+      } catch (error) {
+        toast({
+          title: error.message,
+        });
+      }
+    })();
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await getAllPosts()
-                setPosts(response)
-            } catch (error) {
-                toast({
-                    title: error.message
-                })
-            }
-        })();
+    (async () => {
+      try {
+        const response = await getMostCommentedPosts();
+        setPosts(response);
+      } catch (error) {
+        toast({
+          title: error.message,
+        });
+      }
+    })();
+  }, []);
 
-        (async () => {
-            try {
-                const response = await getMostCommentedPosts()
-                setPosts(response)
-            } catch (error) {
-                toast({
-                    title: error.message
-                })
-            }
-        })();
-    }, []);
-
-    const postToShow = posts.slice(0, showCount).map((el) => { // Use showCount instead of 10
-        return <PostCard key={el.title} {...el} />
-    })
-
-    const loadMore = () => { // New function to load more posts
-        setShowCount(prevCount => prevCount + 10);
-    }
-
+  const postToShow = posts.slice(0, showCount).map((el) => {
     return (
-        <div>
-            {postToShow}
-            {isLoggedIn && showCount < posts.length && (<Button onClick={loadMore} className=''>Load More</Button>)}
-        </div>
-    )
+      <PostCard
+        key={el.title}
+        {...el}
+      />
+    );
+  });
+
+  const loadMore = () => {
+    setShowCount((prevCount) => prevCount + 10);
+  };
+
+  return (
+    <div>
+      {postToShow}
+      {isLoggedIn && showCount < posts.length && (
+        <Button
+          onClick={loadMore}
+          className=""
+        >
+          Load More
+        </Button>
+      )}
+    </div>
+  );
 }
